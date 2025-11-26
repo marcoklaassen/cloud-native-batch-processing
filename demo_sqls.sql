c1644164f865f2d22b3b94f9d4ebf7d11eb64e35
Marco Klaassen <mklaasse@redhat.com>
2025-11-26 15:37:00 +0100

    add permission to maria

--- demo_sqls.sql
+++ demo_sqls.sql
@@ -1,11 +1,15 @@
 -- Access mariadb container
 -- oc exec -n streaming-demo -it mariadb -- /bin/bash
 
+-- mariadb -uroot
+GRANT SELECT, RELOAD, SHOW DATABASES, REPLICATION SLAVE, REPLICATION CLIENT ON *.* TO 'maria';
+
+
 -- Login to mariadb database
 -- mariadb -umaria -pmaria
 
 -- create tables and rows
--- use mariadb;
+use mariadb;
 
 create table customer(
     customer_id int auto_increment,
